7daa29c756be505573cbae76285c234890470341
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-02-28 18:34:28 +0530

    upsdated

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  
+  menu(){
+  let nav = document.getElementById("navbar");
+  console.log("work")
+  
+  if(nav?.classList.contains("shownav")){
+    nav.classList.remove("shownav")
+  }
+  else{
+    nav?.classList.add("shownav")
+  }
+}
+
 }
